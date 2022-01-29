6100ab2c730a5c0fa1105091a334d7eb8172b203
Lelton Borges <leltondev@gmail.com>
2022-01-29 15:02:01 -0300

    @Captor, captor object interno in method

--- src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
+++ src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
@@ -3,19 +3,25 @@ package br.com.alura.leilao.service;
 import br.com.alura.leilao.dao.PagamentoDao;
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Pagamento;
 import br.com.alura.leilao.model.Usuario;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 class GeradorDePagamentoTest {
 
     @Mock
     private PagamentoDao pagamentoDao;
+
+    // Captura uma objeto criado internamente no metodo
+    @Captor
+    private ArgumentCaptor<Pagamento> captorPagamento;
+
     private GeradorDePagamento geradorDePagamento;
 
     @BeforeEach
@@ -29,7 +35,13 @@ class GeradorDePagamentoTest {
         Leilao leilao = leilao();
         Lance vencedor = leilao.getLanceVencedor();
         geradorDePagamento.gerarPagamento(vencedor);
-        Mockito.verify(pagamentoDao).salvar();
+        Mockito.verify(pagamentoDao).salvar(captorPagamento.capture());
+        Pagamento pagamento = captorPagamento.getValue();
+        assertEquals(LocalDate.now().plusDays(1), pagamento.getVencimento());
+        assertEquals(vencedor.getValor(), pagamento.getValor());
+        assertFalse(pagamento.getPago());
+        assertEquals(vencedor.getUsuario(), pagamento.getUsuario());
+        assertEquals(vencedor.getLeilao(), pagamento.getLeilao());
     }
 
     private Leilao leilao(){
