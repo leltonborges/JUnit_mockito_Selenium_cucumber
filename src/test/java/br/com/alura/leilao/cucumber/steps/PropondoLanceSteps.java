e8fca4e423c0da05109528d783a63c372d675dde
Lelton Borges <leltondev@gmail.com>
2022-02-05 18:01:43 -0300

    Cucumber with parameters

--- src/test/java/br/com/alura/leilao/cucumber/steps/PropondoLanceSteps.java
+++ src/test/java/br/com/alura/leilao/cucumber/steps/PropondoLanceSteps.java
@@ -3,11 +3,14 @@ package br.com.alura.leilao.cucumber.steps;
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Leilao;
 import br.com.alura.leilao.model.Usuario;
+import io.cucumber.java.Before;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -17,11 +20,17 @@ public class PropondoLanceSteps {
     private Leilao leilao;
     private Lance lance2;
     private Lance lance3;
+    private List<Lance> lista;
+
+    @Before
+    public void setup(){
+        this.lista = new ArrayList<>();
+        leilao = new Leilao("Phone &");
+    }
 
     @Dado("um lance valido")
     public void dado_um_lance_valido() {
         lance1 = new Lance(new Usuario("Foo"), BigDecimal.TEN);
-        leilao = new Leilao("Notebook ABC");
     }
 
     @Quando("propoe ao leilao")
@@ -35,26 +44,31 @@ public class PropondoLanceSteps {
         assertEquals(BigDecimal.TEN, leilao.getLances().get(0).getValor());
     }
 
-    @Dado("varios lances validos")
-    public void varios_lances_validos() {
-        lance1 = new Lance(new Usuario("Foo"), BigDecimal.TEN);
-        lance2 = new Lance(new Usuario("Bar"), BigDecimal.valueOf(11));
-        lance3 = new Lance(new Usuario("FooBar"), BigDecimal.valueOf(11.2));
-        leilao = new Leilao("Phone &");
+//    @Dado("varios lances validos")
+//    public void varios_lances_validos() {
+//        lance1 = new Lance(new Usuario("Foo"), BigDecimal.TEN);
+//        lance2 = new Lance(new Usuario("Bar"), BigDecimal.valueOf(11));
+//        lance3 = new Lance(new Usuario("FooBar"), BigDecimal.valueOf(11.2));
+//        leilao = new Leilao("Phone &");
+//    }
+
+    @Dado("um lance de {double} reais do usuario {string}")
+    public void um_lance_de_reais_do_usuario_foo(Double amount, String name) {
+        lance1 = new Lance(new Usuario(name), new BigDecimal(amount));
+        lista.add(lance1);
+
     }
 
     @Quando("propoe varios ao lailao")
     public void propoe_varios_ao_lailao() {
-        leilao.propoe(lance1);
-        leilao.propoe(lance2);
-        leilao.propoe(lance3);
+        lista.forEach(leilao::propoe);
     }
     @Entao("os lances sao aceitos")
     public void os_lances_sao_aceitos() {
-        assertEquals(3, leilao.getLances().size());
-        assertEquals(BigDecimal.TEN, leilao.getLances().get(0).getValor());
-        assertEquals(BigDecimal.valueOf(11), leilao.getLances().get(1).getValor());
-        assertEquals(BigDecimal.valueOf(11.2), leilao.getLances().get(2).getValor());
+        assertEquals(lista.size(), leilao.getLances().size());
+        assertEquals(lista.get(0).getValor(), leilao.getLances().get(0).getValor());
+        assertEquals(lista.get(1).getValor(), leilao.getLances().get(1).getValor());
+        assertEquals(lista.get(2).getValor(), leilao.getLances().get(2).getValor());
     }
 
 }
--- src/test/resources/features/propondo_lances.feature
+++ src/test/resources/features/propondo_lances.feature
@@ -8,6 +8,8 @@ Funcionalidade: Propondo lances ao leilao
     Entao lance eh aceito
 
   Cenario: Propondo varios lances
-    Dado varios lances validos
+    Dado um lance de 10.00 reais do usuario "Foo"
+    E um lance de 11.00 reais do usuario "Bar"
+    E um lance de 11.12 reais do usuario "FooBar"
     Quando propoe varios ao lailao
     Entao os lances sao aceitos
